1009ab5400088a96c8397a7989e6471c3f8577c0
BK <betamemo@BK.local>
2023-10-08 23:08:13 +0700

    Update

---
+++ go_with_arrow.py
@@ -0,0 +1,29 @@
+import turtle as t
+
+bob = t.Turtle()
+screen = t.Screen()
+screen.listen()
+
+
+def go_up():
+    bob.setheading(90)
+    bob.forward(10)
+
+
+def go_down():
+    bob.setheading(270)
+    bob.forward(10)
+
+def go_left():
+    bob.setheading(180)
+    bob.forward(10)
+
+def go_right():
+    bob.setheading(0)
+    bob.forward(10)
+
+screen.onkeypress(fun=go_up, key='Up')
+screen.onkeypress(fun=go_down, key='Down')
+screen.onkeypress(fun=go_left, key='Left')
+screen.onkeypress(fun=go_right, key='Right')
+bob.screen.mainloop()
---
+++ painting.py
@@ -0,0 +1,33 @@
+import random
+import turtle as tur
+
+import colorgram
+
+cg = colorgram.extract('img/image.jpeg', 25)
+colors = []
+for color in cg:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    rgb = (r, g, b)
+    colors.append(rgb)
+
+t = tur.Turtle()
+t.penup()
+t.screen.colormode(255)
+t.goto(-250, 250)
+
+for i in range(1, 26):
+    t.dot(80, random.choice(colors))
+    t.forward(120)
+    if i % 5 == 0:
+        t.setheading(270)
+        t.forward(120)
+
+        if i % 10 == 0:
+            t.setheading(0)
+        else:
+            t.setheading(180)
+        t.forward(120)
+
+t.screen.mainloop()
